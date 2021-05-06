66412d7dff02fb218f2560e3f7d962355c71b820
michaelskyba <michaelskyba1411@gmail.com>
2021-05-06 10:07:07 -0400

    auto_config, auto_questions, sample question generation

--- main.py
+++ main.py
@@ -3,6 +3,76 @@
 import curses
 import time
 import datetime
+import os
+import random
+
+def get_question(skill):
+    # List of lines in the file that are for this skill
+    skill_lines = []
+
+    # Sample question file line: "2.2 M -10 -2 -10 -2"
+    with open("auto_questions") as auto_questions:
+        lines = auto_questions.readlines()
+
+        # Find the correct lines for the skill
+        for line in lines:
+            if line[:3] == skill:
+                skill_lines.append(line[4:])
+
+    # Pick a random question type (multi-variable for readability)
+    question_id = random.randint(0, len(skill_lines) - 1)
+    question_line = skill_lines[question_id]
+    question_line = question_line.split()
+
+    # Addition
+    if question_line[0] == "A":
+        question = "Sample addition question (10)"
+        answer = 10
+
+    # Subtraction
+    elif question_line[0] == "S":
+        question = "Sample subtraction question (10)"
+        answer = 10
+
+    # Multiplication
+    elif question_line[0] == "M":
+        question = "Sample multiplication question (10)"
+        answer = 10
+
+    # Division
+    elif question_line[0] == "D":
+        question = "Sample division question (10)"
+        answer = 10
+
+    # Exponents
+    elif question_line[0] == "E":
+        question = "Sample exponent question (10)"
+        answer = 10
+
+    # Roots
+    elif question_line[0] == "R":
+        question = "Sample root question (10)"
+        answer = 10
+
+    return {
+            "question": question,
+            "answer": answer
+            }
+
+def configure(skill):
+    with open("auto_config") as auto_config:
+        lines = auto_config.readlines()
+
+        # Find the correct line for the skill
+        # Sample skill config line: "2.3 25 0.75 13"
+        for line in lines:
+            if line[:3] == skill:
+                config = line[3:].split()
+                return {
+                        "total_time": config[0],
+                        "decrement": config[1],
+                        "threshold": config[2]
+                        }
 
 # Print some text
 def text(text, y, stdscr):
@@ -42,10 +112,13 @@ def main_menu(stdscr, sel_row):
 
 def main(stdscr):
     # Set up colours
+    curses.use_default_colors()
     curses.curs_set(0)
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
     stdscr.nodelay(True)
+
+    skill = "1.1"
 
     # Starting screen
     state = "main_menu"
@@ -80,11 +153,16 @@ def main(stdscr):
                 if sel_row == 1:
                     state = "auto"
 
-                    question = "16 - 14"
-                    current_value = ""
+                    question_object = get_question(skill)
+                    question = question_object["question"]
 
-                    sec_total = 60
-                    sec_rem = 60
+                    config_object = configure(skill)
+                    sec_total = int(config_object["total_time"])
+                    sec_rem = sec_total
+                    decrement = config_object["decrement"]
+                    threshold = config_object["threshold"]
+
+                    current_value = ""
 
                     question_start = datetime.datetime.now()
 
@@ -96,16 +174,41 @@ def main(stdscr):
 
         # Auto Mode
         elif state == "auto":
+            # Calculate bar numbers
             sec_rem = sec_total - (datetime.datetime.now() - question_start).seconds
             bars = round(sec_rem / sec_total * 20)
 
+            # Clear old number
             if sec_rem < 10:
                 sec_rem = " {}".format(sec_rem)
 
-            text("{} = ?".format(question), -1, stdscr)
-            text("> {}_".format(current_value), 0, stdscr)
+            # Skill
+            text(str(skill), -5, stdscr)
 
+            # Question
+            text("{} = ?".format(question), -1, stdscr)
+
+            # Respose
+            text("{}> {}_{}".format(" "*12, current_value, " "*12), 0, stdscr)
+
+            # Time bar
             text("|{bar}{space}|".format(bar="█"*bars, space=" "*(20-bars)), 3, stdscr)
+
+            # Time text
             text("{sec}s left".format(sec=sec_rem), 4, stdscr)
+
+            # text(configure(skill), 6, stdscr)
+
+            # 0-9
+            if key in range(48, 58):
+                current_value += str(key - 48)
+
+                # Make sure value doesn't exceed space
+                if len(current_value) > 6:
+                    current_value = current_value[:-1]
+
+            # Backspace
+            elif key in [8, curses.KEY_BACKSPACE]:
+                current_value = current_value[:-1]
 
 curses.wrapper(main)
