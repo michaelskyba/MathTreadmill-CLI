f54f674c91a70c78eab1d6ef3a0d95ec1ab29c35
michaelskyba <michaelskyba1411@gmail.com>
2021-05-21 13:54:01 -0400

    lol figured out that I did something very wrong which caused it to use 100% CPU

--- main.py
+++ main.py
@@ -168,8 +168,6 @@ def main(stdscr):
     curses.curs_set(0)
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
-    stdscr.nodelay(True)
-
     skills = ["1.1", "1.2", "1.3", "1.4", "2.1", "2.2", "2.3", "2.4", "3.1", "3.2", "3.3", "3.4", "4.1", "4.2", "4.3", "4.4", "5.1", "5.2", "5.3", "5.4", "SSS"]
 
     # Decide whether or not to create the file
